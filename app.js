7e0e260a023c852233698a7d1c9f16bd5c426f7b
Davide Gonnelli <davide.gonnelli1@gmail.com>
2025-03-05 18:57:06 +0100

    aggiunta rotta per la recensione

--- app.js
+++ app.js
@@ -13,3 +13,22 @@ app.use("/covers", express.static("movies_cover"));
 app.listen(3000, () => {
   console.log("✅ Server in ascolto su http://localhost:3000");
 });
+
+// Aggiunge una nuova recensione al film con ID = id
+app.post("/films/:id/reviews", (req, res) => {
+  const filmId = req.params.id;
+  const { review_text, rating } = req.body;
+
+  const query = `
+        INSERT INTO reviews (film_id, review_text, rating) 
+        VALUES (?, ?, ?)
+    `;
+
+  db.query(query, [filmId, review_text, rating], (err, result) => {
+    if (err) {
+      console.error("Errore nell'inserimento recensione:", err);
+      return res.status(500).json({ error: "Errore server" });
+    }
+    res.status(201).json({ message: "Recensione salvata con successo!" });
+  });
+});
